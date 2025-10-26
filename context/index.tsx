ae607d82c1dfc0bd2eee273feebf8753d2d8616f
Nino Keldishvili <Ninokeldishvili@gmail.com>
2025-10-26 17:58:28 +0400

    Update some configurations

--- context/index.tsx
+++ context/index.tsx
@@ -3,7 +3,7 @@
 import { wagmiAdapter, projectId } from '@/config'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createAppKit } from '@reown/appkit/react'
-import { mainnet, arbitrum } from '@reown/appkit/networks'
+import { mainnet, sepolia } from '@reown/appkit/networks'
 import React, { type ReactNode } from 'react'
 import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi'
 
@@ -16,8 +16,8 @@ if (!projectId) {
 
 // Set up metadata
 const metadata = {
-  name: 'appkit-example',
-  description: 'AppKit Example',
+  name: 'moonpie',
+  description: 'Moonpie daily journal app',
   url: 'https://appkitexampleapp.com', // origin must match your domain & subdomain http://localhost:3000/
   icons: ['https://avatars.githubusercontent.com/u/179229932']
 }
@@ -26,11 +26,15 @@ const metadata = {
 const modal = createAppKit({
   adapters: [wagmiAdapter],
   projectId,
-  networks: [mainnet, arbitrum],
-  defaultNetwork: mainnet,
+  networks: [mainnet, sepolia],
+  defaultNetwork: sepolia,
   metadata: metadata,
   features: {
-    analytics: true // Optional - defaults to your Cloud configuration
+    email: false,
+    analytics: true, // Optional - defaults to your Cloud configuration,
+    socials: [
+    ],
+    emailShowWallets: true, // default to true
   }
 })
 
