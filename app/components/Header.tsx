743a10011203dd97327b2a55c174470cd5a119c9
Nino Keldishvili <Ninokeldishvili@gmail.com>
2025-10-28 20:33:55 +0400

    Acc account button if connected

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { modal } from "@/context/";
+import { useAppKitAccount } from "@reown/appkit/react";
 
 export const Header = () => {
+  const { isConnected } = useAppKitAccount();
   return (
     <header className="sticky top-0 z-50 backdrop-blur-md shadow-md border-b border-b-border">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-20 flex items-center justify-between">
@@ -9,13 +11,17 @@ export const Header = () => {
         <div className="shrink-0">
           <span className="text-xl font-extrabold tracking-wider">Buchkie</span>
         </div>
-        <button
-          className="border border-primary text-primary rounded-none py-2 px-6
-          text-sm cursor-pointer hover:shadow-lg shadow-primary hover:scale-[0.99] active:scale-[0.99] transition-all duration-300 ease-in-out"
-          onClick={() => modal.open()}
-        >
-          Connect wallet
-        </button>
+        {isConnected ? (
+          <appkit-account-button />
+        ) : (
+          <button
+            className="border border-primary text-primary rounded-none py-2 px-6
+          text-sm cursor-pointer hover:scale-[0.99] active:scale-[0.99] transition-all duration-300 ease-in-out"
+            onClick={() => modal.open()}
+          >
+            Connect wallet
+          </button>
+        )}
       </div>
     </header>
   );
