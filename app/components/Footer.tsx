ce9b5de1f78fe1a7ae6bb6386c01b8b152a9df9a
Nino Keldishvili <Ninokeldishvili@gmail.com>
2025-10-28 20:46:59 +0400

    Logo and footer spelling correction

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     <footer className="w-full mt-10 border-t border-t-border">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 text-center">
         <p className="text-sm text-primary">
-          &copy; {currentYear} Buckie. All rights reserved.
+          &copy; {currentYear} Buchkie. All rights reserved.
         </p>
       </div>
     </footer>
