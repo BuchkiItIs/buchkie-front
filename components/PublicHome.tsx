1524113e8d84b83353b5fc6b1e9005ae4372e126
Nino Keldishvili <Ninokeldishvili@gmail.com>
2025-10-29 21:29:41 +0400

    Responsive

--- components/PublicHome.tsx
+++ components/PublicHome.tsx
@@ -42,10 +42,10 @@ export default function PublicHome() {
       <Link className="animate-pulse absolute -top-1 right-24 sm:right-20 w-8 h-8 xl:top-0 lg:right-3 xl:w-12 xl:h-12" />
       <Circle className="absolute top-14 right-24 md:top-18 w-3 h-3 md:right-60 lg:w-4 lg:h-4 lg:top-6 xl:w-12 xl:h-12 xl:right-70" />
       <Circle className="animate-pulse absolute top-28 left-2 w-2 h-2 lg:top-12 lg:left-46 xl:left-64 xl:top-5 xl:w-8 xl:h-8" />
-      <Triangle className="absolute top-52 left-4 w-4 h-4 xl:w-8 xl:h-8" />
+      <Triangle className="absolute top-52 left-4 w-4 h-4 xl:w-8 xl:h-8 xl:-left-3" />
       <ScrollText className="absolute sm:bottom-8 lg:bottom-18 bottom-40 left-4 sm:left-36 w-10 h-10 lg:left-24 md:w-12 md:h-12 -rotate-12" />
       <Triangle className="animate-pulse absolute bottom-6 sm:-bottom-2 left-40 sm:left-34 rotate-45 w-6 h-6 lg:bottom-44 lg:left-30" />
-      <Triangle className="absolute bottom-2 sm:bottom-38 lg:bottom-24 right-4 w-2 h-2 lg:right-32 xl:w-8 xl:h-8 xl:bottom-14" />
+      <Triangle className="absolute bottom-2 sm:bottom-38 lg:bottom-24 right-4 w-2 h-2 lg:right-32 xl:w-8 xl:h-8 xl:bottom-14 xl:right-50" />
       <Triangle className="animate-pulse absolute sm:bottom-1 bottom-16 -right-2 sm:right-64 w-10 h-10 md:right-80 lg:right-68 lg:w-10 lg:h-10" />
       <Pencil className="absolute bottom-68 sm:top-15 lg:top-50 lg:right-16 w-8 h-8 sm:right-68 right-6 xl:right-40 xl:top-60 xl:w-14 xl:h-14" />
 
@@ -62,7 +62,7 @@ export default function PublicHome() {
         <Skeleton className="h-4 w-full" />
       </SkeletonCard>
 
-      <SkeletonCard className="bottom-72 left-2 w-56 xl:-rotate-2 rotate-12 sm:-top-10 sm:left-50 lg:w-50 lg:top-auto lg:bottom-5 xl:w-100 xl:top-auto xl:bottom-10">
+      <SkeletonCard className="bottom-72 left-2 w-56 xl:-rotate-2 rotate-12 sm:left-50 sm:bottom-30 lg:w-50 lg:bottom-5 xl:bottom-10 xl:w-60">
         <div className="flex items-center space-x-4">
           <Skeleton className="h-12 w-12 xl:w-20 xl:h-20" />
           <div className="flex-1 space-y-2">
@@ -72,7 +72,7 @@ export default function PublicHome() {
         </div>
       </SkeletonCard>
 
-      <SkeletonCard className="bottom-24 sm:-bottom-10 right-2 w-54 -rotate-12 md:-bottom-35 md:-right-12 lg:right-0 xl:-right-12 xl:bottom-30 md:w-80 lg:w-60 xl:w-110">
+      <SkeletonCard className="bottom-24 sm:-bottom-10 right-2 w-54 -rotate-12 md:-bottom-35 md:-right-12 lg:right-0 xl:-right-12 xl:bottom-10 md:w-80 lg:w-60 xl:w-60">
         <Skeleton className="h-12 w-full md:h-30 lg:h-24" />
         <div className="space-y-2">
           <Skeleton className="h-4 w-5/6" />
